bd56174e255bb744f5c52d6d1ebad789b7f93503
danyu rajbahak <rajbahakdanyu@gmail.com>
2020-12-20 10:53:02 +0545

    Added update form functionality

--- Edit Form.cs
+++ Edit Form.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Globalization;
 
 namespace AppDev_CW_dNF
 {
@@ -87,7 +88,34 @@ namespace AppDev_CW_dNF
         // Save changes to file
         private void btnSave_Click(object sender, EventArgs e)
         {
+            string workingDirectory = Environment.CurrentDirectory; // Get the current WORKING directory
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName; // Get the current PROJECT directory
+            string filePath = projectDirectory + @"\AppDev_CW_dNF\fields.txt"; // Get the full file path
 
+            string current;
+            string field;
+
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo; // Creates a TextInfo based on the "en-US" culture.
+            StreamWriter writer = new StreamWriter(filePath);
+
+            int numRows = reviewFields.Rows.Count; // Get total number of rows in dataGridView
+            int count = 0;
+
+            foreach (DataGridViewRow row in reviewFields.Rows) // loop through all rows of dataGridView
+            {
+                current = row.Cells[0].Value.ToString().ToLower(); // Get value of current cell of row
+                field = textInfo.ToTitleCase(current);
+
+                if (count < numRows-1) // Check if it is the last field
+                {
+                    field = field + ",";
+                }
+                writer.Write(field); // Write to file               
+                count += 1;
+            }
+            writer.Close();
+           
+            MessageBox.Show("Changes saved successfully.", "Success");
         }
     }
 }
